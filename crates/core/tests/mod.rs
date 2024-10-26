d28a99cd70b90d46d120641ef1d0a2068d97f0c5
gibbz00 <gabrielhansson00@gmail.com>
2024-10-26 09:24:21 +0200

    feat(macros): ToXml for unit structs

--- crates/core/tests/mod.rs
+++ crates/core/tests/mod.rs
@@ -5,3 +5,28 @@ fn error_ui() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/ui/*.rs");
 }
+
+pub use test_utils::assert_serialize_str;
+mod test_utils {
+    pub fn assert_serialize_str<T: ximple::ToXml>(expected_str: &str, t: &T) {
+        let actual = ximple::to_string(t).unwrap();
+        pretty_assertions::assert_eq!(expected_str, actual);
+    }
+}
+
+mod structs {
+    use ximple::ToXml;
+
+    mod unit {
+        use super::*;
+        use crate::assert_serialize_str;
+
+        #[derive(ToXml)]
+        struct UnitStruct;
+
+        #[test]
+        fn unit_noop() {
+            assert_serialize_str("", &UnitStruct);
+        }
+    }
+}
--- crates/macros/src/derive_to_xml.rs
+++ crates/macros/src/derive_to_xml.rs
@@ -1,18 +1,45 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, Data, DeriveInput};
+use proc_macro2::TokenStream as TokenStream2;
+use syn::{parse_macro_input, Data, DataEnum, DataStruct, DeriveInput, Fields, Ident};
 
 pub fn derive_to_xml_impl(token_stream: TokenStream) -> TokenStream {
     let type_definition = parse_macro_input!(token_stream as DeriveInput);
 
+    let ident = type_definition.ident;
+
     match type_definition.data {
-        Data::Struct(data_struct) => todo!(),
-        Data::Enum(data_enum) => todo!(),
-        Data::Union(data_union) => {
-            return syn::Error::new(data_union.union_token.span, "unions are not supported")
-                .to_compile_error()
-                .into()
+        Data::Struct(data_struct) => structs::derive(ident, data_struct),
+        Data::Enum(data_enum) => derive_enum(data_enum),
+        Data::Union(data_union) => syn::Error::new(data_union.union_token.span, "unions are not supported").to_compile_error(),
+    }
+    .into()
+}
+
+mod structs {
+
+    use quote::quote;
+
+    use super::*;
+
+    pub fn derive(ident: Ident, data_struct: DataStruct) -> TokenStream2 {
+        match data_struct.fields {
+            Fields::Named(fields_named) => todo!(),
+            Fields::Unnamed(fields_unnamed) => todo!(),
+            Fields::Unit => unit(ident),
         }
     }
 
+    fn unit(ident: Ident) -> TokenStream2 {
+        quote! {
+            impl ::ximple::ToXml for #ident {
+                fn serialize(&self, serializer: &mut ::ximple::ser::Serializer<impl std::io::Write>) -> Result<(), ::ximple::ser::Error> {
+                    Ok(())
+                }
+            }
+        }
+    }
+}
+
+fn derive_enum(data_enum: DataEnum) -> TokenStream2 {
     todo!()
 }
