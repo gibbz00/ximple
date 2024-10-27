458c534c52dce27a02d480a0ffaa9837723d55be
gibbz00 <gabrielhansson00@gmail.com>
2024-10-27 22:10:32 +0100

    feat(macros): sketch enum deserialization

--- crates/core/tests/derives/enums.rs
+++ crates/core/tests/derives/enums.rs
@@ -7,100 +7,56 @@ mod unit {
         B,
     }
 
-    assert_bijective_xml!("<A/>", UnitEnum::A);
-
-    #[test]
-    fn element_for_variant() {
-        assert_serialize_str("<A/>", &UnitEnum::A);
-        assert_serialize_str("<B/>", &UnitEnum::B);
-    }
+    assert_bijective_xml!(variant_a, "<A/>", UnitEnum::A);
+    assert_bijective_xml!(variant_b, "<B/>", UnitEnum::B);
 }
 
 mod named {
     use crate::*;
 
-    #[derive(ximple::ToXml)]
+    #[derive(Debug, PartialEq, ximple::ToXml, ximple::FromXml)]
     enum NamedEnum {
-        A { a: (), b: &'static str },
+        A { a: (), b: bool },
     }
 
-    #[test]
-    fn element_for_variant_and_fields() {
-        let named_enum = NamedEnum::A { a: (), b: "test" };
-        assert_serialize_str("<A><a/><b>test</b></A>", &named_enum);
-    }
+    assert_bijective_xml!("<A><a/><b>false</b></A>", NamedEnum::A { a: (), b: false });
 }
 
 mod unnamed {
     use crate::*;
 
-    #[derive(ximple::ToXml)]
+    #[derive(Debug, PartialEq, ximple::ToXml, ximple::FromXml)]
     enum UnnamedEnum {
         A(InnerUnnamedEnum, InnerUnnamedEnum),
     }
 
-    #[derive(ximple::ToXml)]
+    #[derive(Debug, PartialEq, ximple::ToXml, ximple::FromXml)]
     enum InnerUnnamedEnum {
-        B(&'static str),
-        C(&'static str),
+        B(bool),
+        C(bool),
     }
 
-    #[test]
-    fn element_for_variant_but_not_fields() {
-        let unnamed_enum = UnnamedEnum::A(InnerUnnamedEnum::B("bb"), InnerUnnamedEnum::C("cc"));
-        assert_serialize_str("<A><B>bb</B><C>cc</C></A>", &unnamed_enum);
-    }
+    assert_bijective_xml!(
+        "<A><B>true</B><C>false</C></A>",
+        UnnamedEnum::A(InnerUnnamedEnum::B(true), InnerUnnamedEnum::C(false))
+    );
 }
 
 mod mixed {
     use crate::*;
 
-    #[derive(ximple::ToXml)]
+    #[derive(Debug, PartialEq, ximple::ToXml, ximple::FromXml)]
     enum Mixed {
         A,
         B { b: bool },
         C(Box<Mixed>, Box<Mixed>),
     }
 
-    impl ::ximple::FromXml for Mixed {
-        fn deserialize(deserializer: &mut ::ximple::de::Deserializer<impl std::io::Read>) -> Result<Self, ::ximple::de::Error> {
-            let variant_name = ::ximple::types::Name::new("A");
-            if deserializer.peek_element(variant_name) {
-                deserializer.read_element::<()>(variant_name)?;
-                return Ok(Mixed::A);
-            }
-
-            let variant_name = ::ximple::types::Name::new("B");
-            if deserializer.peek_element(variant_name) {
-                deserializer.read_start_element(variant_name);
-
-                let value = Mixed::B { b: deserializer.read_element(::ximple::types::Name::new("b"))? };
-
-                deserializer.read_end_element(variant_name);
-
-                return Ok(value);
-            }
-
-            if deserializer.peek_element(::ximple::types::Name::new("C")) {
-                return Ok(Mixed::C(
-                    ::ximple::FromXml::deserialize(deserializer)?,
-                    ::ximple::FromXml::deserialize(deserializer)?,
-                ));
-            }
-
-            Err(::ximple::de::Error::element_not_found(["A", "B", "C"].map(ToString::to_string)))
-        }
-    }
-
-    #[test]
-    fn element_and_each_field() {
-        let mixed_enum = Mixed::A;
-        assert_serialize_str("<A/>", &mixed_enum);
-
-        let mixed_enum = Mixed::B { b: true };
-        assert_serialize_str("<B><b>true</b></B>", &mixed_enum);
-
-        let mixed_enum = Mixed::C(Box::new(Mixed::A), Box::new(Mixed::B { b: false }));
-        assert_serialize_str("<C><A/><B><b>false</b></B></C>", &mixed_enum);
-    }
+    assert_bijective_xml!(variant_a, "<A/>", Mixed::A);
+    assert_bijective_xml!(variant_b, "<B><b>true</b></B>", Mixed::B { b: true });
+    assert_bijective_xml!(
+        variant_c,
+        "<C><A/><B><b>false</b></B></C>",
+        Mixed::C(Box::new(Mixed::A), Box::new(Mixed::B { b: false }))
+    );
 }
--- crates/macros/src/derive_from_xml/enums.rs
+++ crates/macros/src/derive_from_xml/enums.rs
@@ -7,41 +7,19 @@ pub fn derive(enum_ident: Ident, data_enum: DataEnum) -> TokenStream2 {
     // attributes into account too.
     let not_found_error = {
         let variant_names = data_enum.variants.iter().map(|variant| variant.ident.to_string());
-        quote! { Err(::ximple::de::Error::element_not_found([#(#variant_names),*].map(ToString::to_string))); }
+        quote! { Err(::ximple::de::Error::element_not_found([#(#variant_names),*].map(ToString::to_string))) }
     };
 
-    let match_arms = data_enum.variants.into_iter().map(|variant| match variant.fields {
-        syn::Fields::Named(fields_named) => named(&enum_ident, &variant.ident, fields_named),
-        syn::Fields::Unnamed(fields_unnamed) => unnamed(&enum_ident, &variant.ident, fields_unnamed),
-        syn::Fields::Unit => unit(&enum_ident, &variant.ident),
+    let variant_deserializers = data_enum.variants.into_iter().map(|variant| match variant.fields {
+        syn::Fields::Named(fields_named) => named(&variant.ident, fields_named),
+        syn::Fields::Unnamed(fields_unnamed) => unnamed(&variant.ident, fields_unnamed),
+        syn::Fields::Unit => unit(&variant.ident),
     });
 
     quote! {
-    impl ::ximple::FromXml for #enum_ident {
+        impl ::ximple::FromXml for #enum_ident {
             fn deserialize(deserializer: &mut ::ximple::de::Deserializer<impl std::io::Read>) -> Result<Self, ::ximple::de::Error> {
-                let variant_name = ::ximple::types::Name::new("A");
-                if deserializer.peek_element(variant_name) {
-                    deserializer.read_element::<()>(variant_name)?;
-                    return Ok(Mixed::A);
-                }
-
-                let variant_name = ::ximple::types::Name::new("B");
-                if deserializer.peek_element(variant_name) {
-                    deserializer.read_start_element(variant_name);
-
-                    let value = Mixed::B { b: deserializer.read_element(::ximple::types::Name::new("b"))? };
-
-                    deserializer.read_end_element(variant_name);
-
-                    return Ok(value);
-                }
-
-                if deserializer.peek_element(::ximple::types::Name::new("C")) {
-                    return Ok(Mixed::C(
-                        ::ximple::FromXml::deserialize(deserializer)?,
-                        ::ximple::FromXml::deserialize(deserializer)?,
-                    ));
-                }
+                #(#variant_deserializers)*
 
                 #not_found_error
             }
@@ -49,14 +27,75 @@ pub fn derive(enum_ident: Ident, data_enum: DataEnum) -> TokenStream2 {
     }
 }
 
-fn unit(enum_ident: &Ident, variant_ident: &Ident) -> TokenStream2 {
-    todo!()
+fn unit(variant_ident: &Ident) -> TokenStream2 {
+    let variant_name_variable = utils::variant_name_variable(variant_ident);
+
+    quote! {
+        #variant_name_variable
+        if deserializer.peek_start_element_matches(variant_name) {
+            deserializer.read_element::<()>(variant_name)?;
+            return Ok(Self::#variant_ident);
+        }
+    }
 }
 
-fn unnamed(enum_ident: &Ident, variant_ident: &Ident, fields: FieldsUnnamed) -> TokenStream2 {
-    todo!()
+fn unnamed(variant_ident: &Ident, fields: FieldsUnnamed) -> TokenStream2 {
+    let variant_name_variable = utils::variant_name_variable(variant_ident);
+
+    let unnamed_field_deserializers = fields.unnamed.iter().map(|_| {
+        quote! { ::ximple::FromXml::deserialize(deserializer)? }
+    });
+
+    quote! {
+        #variant_name_variable
+        if deserializer.peek_start_element_matches(variant_name) {
+            deserializer.read_start_element::<()>(variant_name)?;
+
+            let value = Self::#variant_ident(#(#unnamed_field_deserializers),*);
+
+            deserializer.read_end_element(variant_name)?;
+
+            return Ok(value);
+        }
+    }
 }
 
-fn named(enum_ident: &Ident, variant_ident: &Ident, fields: FieldsNamed) -> TokenStream2 {
-    todo!()
+fn named(variant_ident: &Ident, fields: FieldsNamed) -> TokenStream2 {
+    let variant_name_variable = utils::variant_name_variable(variant_ident);
+
+    let named_field_deserializers = fields.named.into_iter().map(|field| {
+        let field_ident = field.ident.expect("encountered named fields without identifier");
+        let field_ident_string = field_ident.to_string();
+        quote! {
+            #field_ident: deserializer.read_element(::ximple::types::Name::new(#field_ident_string))?
+        }
+    });
+
+    quote! {
+        #variant_name_variable
+        if deserializer.peek_start_element_matches(variant_name) {
+            deserializer.read_start_element::<()>(variant_name)?;
+
+            let value = Self::#variant_ident {
+                #(#named_field_deserializers),*
+            };
+
+            deserializer.read_end_element(variant_name)?;
+
+            return Ok(value);
+        }
+    }
+}
+
+mod utils {
+    use super::*;
+
+    pub fn variant_name_variable(variant_ident: &Ident) -> TokenStream2 {
+        // TODO: take prefix and rename attributes into account
+        let variant_name = variant_ident.to_string();
+
+        quote! {
+            let variant_name = ::ximple::types::Name::new(#variant_name);
+        }
+    }
 }
